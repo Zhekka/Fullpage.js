be47ecfcaac9f96c92db3676e56d88f41dc2aca1
eugene <kirnasuk1284@gmail.com>
2021-02-05 16:44:57 +0200

    add Fullpage.js scrolling and icons with social netwoks

---
+++ js/main.js
@@ -0,0 +1,9 @@
+new fullpage('#fullPage', {
+    autoScrolling: true,
+    navigation: true,
+    navigationTooltips:[
+        'Home','Service','Personal','App development',
+        'Portfolio','Blog','Pricing','Contact'
+    ],
+    showActiveTooltip:true
+})
